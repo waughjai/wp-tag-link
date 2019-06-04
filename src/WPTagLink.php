b4e54031525ff42ccd4277941e44bd32e920150e
Jaimeson Waugh <jaimeson@4cesi.com>
2019-06-04 13:54:45 -0700

    Update TestHashItem dependency

--- src/WPTagLink.php
+++ src/WPTagLink.php
@@ -1,37 +1,35 @@
 <?php
 
 declare( strict_types = 1 );
-namespace WaughJ\WPTagLink
+namespace WaughJ\WPTagLink;
+
+use \WaughJ\HTMLLink\HTMLLink;
+use WaughJ\TestHashItem\TestHashItem;
+
+class WPTagLink extends HTMLLink
 {
-	use \WaughJ\HTMLLink\HTMLLink;
-	use function WaughJ\TestHashItem\TestHashItemExists;
-	use function WaughJ\TestHashItem\TestHashItemString;
-
-	class WPTagLink extends HTMLLink
+	public function __construct( array $atts )
 	{
-		public function __construct( array $atts )
+		$tag = self::GetTagID( $atts );
+		if ( $tag !== null )
 		{
-			$tag = self::GetTagID( $atts );
-			if ( $tag !== null )
-			{
-				$atts[ 'href' ] = get_tag_link( $tag->term_id );
-				$atts[ 'value' ] = TestHashItemExists( $atts, 'value', $tag->title );
-			}
-			else
-			{
-				$atts[ 'href' ] = TestHashItemExists( $atts, 'href', TestHashItemString( $atts, 'slug', '' ) );
-				$atts[ 'value' ] = TestHashItemExists( $atts, 'value', $atts[ 'href' ] );
-			}
-
-			$href = $atts[ 'href' ];
-			$value = $atts[ 'value' ];
-			parent::__construct( $href, $value, $atts );
+			$atts[ 'href' ] = get_tag_link( $tag->term_id );
+			$atts[ 'value' ] = $atts[ 'value' ] ?? $tag->title;
+		}
+		else
+		{
+			$atts[ 'href' ] = $atts[ 'href' ] ?? TestHashItem::getString( $atts, 'slug', '' );
+			$atts[ 'value' ] = $atts[ 'value' ] ?? $atts[ 'href' ];
 		}
 
-		private static function GetTagID( array $atts )
-		{
-			$tag = ( isset( $atts[ 'slug' ] ) ) ? get_term_by( 'slug', $atts[ 'slug' ], 'post_tag' ) : false;
-			return ( $tag !== false ) ? $tag : null;
-		}
+		$href = $atts[ 'href' ];
+		$value = $atts[ 'value' ];
+		parent::__construct( $href, $value, $atts );
+	}
+
+	private static function GetTagID( array $atts )
+	{
+		$tag = ( isset( $atts[ 'slug' ] ) ) ? get_term_by( 'slug', $atts[ 'slug' ], 'post_tag' ) : false;
+		return ( $tag !== false ) ? $tag : null;
 	}
 }
